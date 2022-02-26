8bd8eee1b47fabe5f6b7c588306edcf3d3eb0ff7
elantry <mohamedahmed25173@gmail.com>
2022-02-26 02:24:16 +0200

    Updates

---
+++ precache-manifest.f257142dba425aa9ee30b893bf295906.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8f729a843836273fb113c11f9f6877e",
+    "url": "/breaking-bad-cast-master/index.html"
+  },
+  {
+    "revision": "6c3f7c736eb649e1b841",
+    "url": "/breaking-bad-cast-master/static/css/main.35400bea.chunk.css"
+  },
+  {
+    "revision": "c9413a2b0e288bcb1e85",
+    "url": "/breaking-bad-cast-master/static/js/2.68f4301c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breaking-bad-cast-master/static/js/2.68f4301c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c3f7c736eb649e1b841",
+    "url": "/breaking-bad-cast-master/static/js/main.2d170a3b.chunk.js"
+  },
+  {
+    "revision": "e0b892579fa3f1b5af74",
+    "url": "/breaking-bad-cast-master/static/js/runtime-main.1ff7c214.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/breaking-bad-cast-master/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/breaking-bad-cast-master/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/breaking-bad-cast-master/static/media/spinner.235b9b3e.gif"
+  }
+]);
